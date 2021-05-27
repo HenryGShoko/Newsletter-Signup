e27181c463914e8132c0f00ab3ebc376b6984cac
HenryGShoko <gshokx91@gmail.com>
2021-05-27 22:15:01 +0200

    adding success and failure pages

--- app.js
+++ app.js
@@ -1,79 +1,67 @@
-const express = require ("express")
-const bodyParser = require ("body-parser") 
-const request =require ("request")
-const https =require ("https")
-const { response } = require("express")
-
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const https = require('https')
 
 const app = express()
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/signup.html")
-})
-
-app.post('/', function (req, res) {
-
-  const firstName = req.body.fName
-  const lastName =req.body.lName
-  const email = req.body.email
-
-const data = {
-  members: [
-    {
-      email_address: email,
-      status: 'subscribed',
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName
-      }
-    }
-  ]
-}
-
-var jsonData = JSON.stringify(data)
-
-const url ='https://us1.api.mailchimp.com/3.0/lists/91aaaf5888'
-
-const options = {
-  method: 'POST',
-  auth: 'henry1:8d8dd2eaa578c6d525aa56cb02e13815-us1'
-
-}
-
-const request = https.request(url, options, (response)=>{
-
-  if (request.statusCode === 200) {
-    res.sendFile(__dirname + '/success.html' )
-  } else {
-    res.sendFile(__dirname + '/failure.html' )
-  }
-
-
-  response.on('data',(data)=>{
-    console.log(JSON.parse(data))
+    res.sendFile(__dirname + '/signup.html')
   })
+
+  app.post('/', (req, res) => {
+
+    const firstName = req.body.fName
+    const lastName = req.body.lName
+    const email = req.body.email
+
+    const data = {
+      members: [
+        {
+          email_address: email,
+          status: 'subscribed',
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+          }
+        }
+      ]
+    }
+
+    const jsonData = JSON.stringify(data)
+
+    const url = 'https://us1.api.mailchimp.com/3.0/lists/91aaaf58889'
+
+    const options = {
+      method: 'POST',
+      auth: 'henry:c103533c0a720621ba8c101886818343-us1'
+    }
+
+    const request = https.request(url, options, (response) => {
+
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + '/success.html')
+      } else {
+        res.sendFile(__dirname + '/failure.html')
+      }
+
+    response.on('data',(data) => {
+      console.log(JSON.parse(data))
+    })
+    })
+
+    request.write(jsonData)
+    request.end();
+  })
+
+  app.post('/failure', (req, res) => {
+    res.redirect('/')
+  })
+
+app.listen(3000, () => {
+console.log('Server is running on port 3000')
 })
-
-request.write(jsonData)
-request.end()
-
-})
-
-app.post("/failure", (req, res) => {
-res.redirect("/")
-})
-
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`server is running on port 3000`)
-})
-
-// API key 
-// 8d8dd2eaa578c6d525aa56cb02e13815-us1
-
-// list Id
-// 91aaaf5888
+  
