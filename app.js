fac06fbf12803c2833a88a4bf0036a924ed9a5df
HenryGShoko <gshokx91@gmail.com>
2021-05-27 22:35:43 +0200

    added Procfile for deployment on Heroku

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.get('/', (req, res) => {
     res.redirect('/')
   })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 console.log('Server is running on port 3000')
 })
   
